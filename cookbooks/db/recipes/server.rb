b0c4415ace00c2a868d09ef9d1f0d402c2f4b67d
sinkingpoint <colin@quirl.co.nz>
2018-08-07 22:07:25 +0100

    Added my.cnf which binds to private ip

--- cookbooks/db/recipes/server.rb
+++ cookbooks/db/recipes/server.rb
@@ -3,5 +3,18 @@ mariadb_repository 'Maria Apt Repo' do
 end
 
 mariadb_server_install 'MariaDB Server install' do
-      version '10.3'
+    version '10.3'
+    notifies :start, 'service[mysql]'
+end
+
+service 'mysql' do
+    action :nothing
+end
+
+template '/etc/mysql/my.cnf' do
+    source 'my.cnf.erb'
+    variables ({
+        listen_ip: (node['network']['interfaces']['eth0']['addresses'].select { |a, details| details[:family] == 'inet'}).keys[0]
+    })
+    notifies :restart, 'service[mysql]'
 end
--- cookbooks/db/test/integration/default/serverspec/mysql_spec.rb
+++ cookbooks/db/test/integration/default/serverspec/mysql_spec.rb
@@ -8,3 +8,9 @@ end
 describe command('mysql --version') do
     its(:stdout) { should match /10.3.8-MariaDB/ }
 end
+
+describe file('/etc/mysql/my.cnf') do
+    it { should exist }
+    it { should be_file }
+    its(:content) { should match /bind-address=[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/ }
+end
