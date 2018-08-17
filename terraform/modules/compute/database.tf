efc9a6390a5da09ef12a7e3aa9f2f7615c0bf388
sinkingpoint <colin@quirl.co.nz>
2018-08-17 10:21:51 +0100

    Added configuration for database backups bucket

--- terraform/modules/compute/database.tf
+++ terraform/modules/compute/database.tf
@@ -1,3 +1,75 @@
+resource "aws_s3_bucket" "database_backups" {
+  bucket = "sinking-database-backups"
+}
+
+resource "aws_iam_policy" "database_policy" {
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "Stmt1534460117429",
+      "Action": [
+        "s3:DeleteObject",
+        "s3:DeleteObjectTagging",
+        "s3:DeleteObjectVersion",
+        "s3:DeleteObjectVersionTagging",
+        "s3:GetObject",
+        "s3:GetObjectAcl",
+        "s3:GetObjectTagging",
+        "s3:GetObjectTorrent",
+        "s3:GetObjectVersion",
+        "s3:GetObjectVersionAcl",
+        "s3:GetObjectVersionForReplication",
+        "s3:GetObjectVersionTagging",
+        "s3:GetObjectVersionTorrent",
+        "s3:GetReplicationConfiguration",
+        "s3:PutObject",
+        "s3:PutObjectAcl",
+        "s3:PutObjectTagging",
+        "s3:PutObjectVersionAcl",
+        "s3:ReplicateObject",
+        "s3:RestoreObject"
+      ],
+      "Effect": "Allow",
+      "Resource": "${aws_s3_bucket.database_backups.arn}/*"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role" "database_role" {
+  name = "database_role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_policy_attachment" "database_policy_role" {
+  name = "database_role_attachment"
+  policy_arn = "${aws_iam_policy.database_policy.arn}"
+  roles = ["${aws_iam_role.database_role.name}"]
+}
+
+resource "aws_iam_instance_profile" "database_profile" {
+  name = "database_profile"
+  role = "${aws_iam_role.database_role.name}"
+}
+
 resource "aws_instance" "database_template" {
   ami = "${data.aws_ami.debian-stretch.id}"
   instance_type = "t2.micro"
@@ -7,5 +79,6 @@ resource "aws_instance" "database_template" {
   user_data = "${file("${path.module}/init_scripts/db.sh")}"
   associate_public_ip_address = true
   key_name = "personal-infra"
+  iam_instance_profile = "${aws_iam_instance_profile.database_profile.name}"
 }
 
