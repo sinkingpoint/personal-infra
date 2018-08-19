87b0c5eef99cb931668c40c7b11698cda4a8756e
sinkingpoint <colin@quirl.co.nz>
2018-08-19 08:32:07 +0100

    Allow egress from public instances

--- terraform/modules/compute/security_groups.tf
+++ terraform/modules/compute/security_groups.tf
@@ -55,4 +55,11 @@ resource "aws_security_group" "public_80" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
