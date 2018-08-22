b547a98cbc3961c9ff85f01f80abd7c44b4f55a6
sinkingpoint <colin@quirl.co.nz>
2018-08-22 18:30:56 +0100

    Added explicit setting of storage backend to docker registry

--- cookbooks/docker-registry/recipes/container.rb
+++ cookbooks/docker-registry/recipes/container.rb
@@ -14,6 +14,7 @@ docker_container 'bookstack' do
   tag '2'
   port '8080:5000'
   env [
+    'REGISTRY_STORAGE=s3',
     'REGISTRY_STORAGE_S3_REGION=eu-west-2',
     "REGISTRY_STORAGE_S3_BUCKET=#{node['docker-registry']['bucket']}"
   ]
