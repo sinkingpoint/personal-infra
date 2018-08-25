2b612ac583977898edf63cd0d91f938433ddf789
sinkingpoint <colin@quirl.co.nz>
2018-08-25 22:35:53 +0100

    Added script to restore grafana db

--- cookbooks/grafana/recipes/backup.rb
+++ cookbooks/grafana/recipes/backup.rb
@@ -2,8 +2,13 @@ directory '/opt/grafana/scripts' do
   recursive true
 end
 
-template '/opt/grafana/scripts/backup.sh' do
+template '/opt/grafana/scripts/backup_db.sh' do
   source 'backup.sh.erb'
+  mode '0700'
+end
+
+template '/opt/grafana/scripts/restore_db.sh' do
+  source 'restore.sh.erb'
   mode '0700'
 end
 
--- cookbooks/grafana/test/integration/default/serverspec/backup_spec.rb
+++ cookbooks/grafana/test/integration/default/serverspec/backup_spec.rb
@@ -1,4 +1,13 @@
-describe file('/opt/grafana/scripts/backup.sh') do
+require 'spec_helper.rb'
+
+describe file('/opt/grafana/scripts/backup_db.sh') do
+  it { should exist }
+  it { should be_executable }
+  it { should be_file }
+  its(:content) { should match /#!\/bin\/bash/ }
+end
+
+describe file('/opt/grafana/scripts/restore_db.sh') do
   it { should exist }
   it { should be_executable }
   it { should be_file }
