c3fe19a0fa8962f2115bf581523de850d14f81e3
sinkingpoint <colin@quirl.co.nz>
2018-08-27 23:14:27 +0100

    Fixed name on prometheus restore script

--- cookbooks/prometheus/recipes/backup.rb
+++ cookbooks/prometheus/recipes/backup.rb
@@ -7,13 +7,13 @@ template '/opt/prometheus/scripts/backup_data.sh' do
   mode 0700
 end
 
-template '/opt/prometheus/scripts/restore_data.sh' do
+template '/opt/prometheus/scripts/restore_db.sh' do
   source 'restore_data.sh.erb'
   mode 0700
 end
 
 cron 'backup_db' do
   command '/opt/prometheus/scripts/backup_data.sh && date +%s > /opt/prometheus/last_backup.txt'
-  hour '*/2'
+  hour '*'
   minute '30'
 end
