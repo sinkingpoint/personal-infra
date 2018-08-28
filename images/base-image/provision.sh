947dd8726f7c13fd5ae9c16969f09f7b2c423816
sinkingpoint <colin@quirl.co.nz>
2018-08-28 21:35:57 +0100

    Added packer build for a base ami that contains the common cookbook

---
+++ images/base-image/provision.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+export HOME=/home/admin
+REPO_PATH=/opt/provision-repo
+
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common
+wget -qO - https://packages.chef.io/chef.asc | sudo apt-key add -
+
+echo "deb https://packages.chef.io/repos/apt/stable stretch main" | sudo tee /etc/apt/sources.list.d/chef-stable.list
+
+sudo apt-get update
+sudo apt-get install -y chefdk git
+
+if [[ ! -d "${REPO_PATH}" ]]; then
+    sudo mkdir "${REPO_PATH}" && sudo chown admin:admin "${REPO_PATH}"
+    git clone https://github.com/sinkingpoint/personal-infra "${REPO_PATH}"
+else
+    cd "${REPO_PATH}" && git pull origin master
+fi
+
+find "${REPO_PATH}/cookbooks" -type f -name Berksfile -exec berks vendor -b {} "${REPO_PATH}/cookbooks" \;
+cd "${REPO_PATH}" && sudo chef-client -z -r 'common'
+
+sudo rm -rf "${REPO_PATH}"
