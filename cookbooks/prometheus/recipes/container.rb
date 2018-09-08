f3c038f4e196a9349bbd48e63aab4d5b96c4a32a
sinkingpoint <colin@quirl.co.nz>
2018-09-08 19:40:35 +0100

    Added back default prometheus data dir

--- cookbooks/prometheus/recipes/container.rb
+++ cookbooks/prometheus/recipes/container.rb
@@ -19,5 +19,5 @@ docker_container 'prometheus' do
     '/opt/prometheus/storage:/prometheus',
     '/opt/prometheus/rules:/rules'
   ]
-  command '--config.file=/etc/prometheus/prometheus.yml --storage.tsdb.retention=7d'
+  command '--config.file=/etc/prometheus/prometheus.yml --storage.tsdb.retention=7d --storage.tsdb.path=/prometheus'
 end
