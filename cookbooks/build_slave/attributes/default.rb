90c731f6e8db6ef8fbcd931dad1f93e6d8e61a2f
sinkingpoint <colin@quirl.co.nz>
2018-09-10 22:26:20 +0100

    Added jenkins user to build slave

---
+++ cookbooks/build_slave/attributes/default.rb
@@ -0,0 +1,16 @@
+override['common']['users'] = [
+  {
+    name: 'colin',
+    shell: '/usr/bin/zsh',
+    ssh_keys: [
+      'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCzny7ZShOnUZMDgFKeyTFB3+uUX2sLS+PWBW5WhgolI3Z4Cdn7xFPYR6WlaAtHj06ddvWFCUnwDEro21mSNSKXrrfYKGqkHRP1ooA5QpIHLgz9+FHbAxe28adKSymQYi7+Drs+NOQws2icIHei1NX2pcUSI0aBNAuZSzdHURQvWpqr63qpZr2ecASK+C+rrz/+7TRSw2mmu8VVEptSpoQEZ/cf/iBUPHDWaW3osD3hIWNsbgdTVT6AaquoCYKRunU1sw9faNphwYIx46IvM2h5JtaOn/Kn7ocnXbiaaG/vqp1AxAtMbAWYMwXZvcsvC+vtX2E2wv9/AvIaB6mo+PuT colin@anatinus'
+    ]
+  },
+  {
+    name: 'jenkins',
+    shell: '/usr/bin/bash',
+    ssh_keys: [
+      'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDXiZ+mJvyM1a5w6budjFObnPS7FD1fkw5ejWiAPU/CbmOhwsBh/KFhVlwJX6MIA8N21pQN4vS4XKxZMan4fhrLhOkayNhy5FYOcBzq7ChcdgqdKaQXjEkorQlzWFFWyMP0hRiFXaDmsAxPrF8WGZPbJ+1JsxAm1w03C1m5YcdKtS6NowEzueAFUzp1jix5iCscmHm6NN+gpiM4+kMMtzbUf5SNvCY0Hxvf4C9jWq7XBX+P9/o2Jc00HJczfq1DG3wFpSo50mRNMEbMz5TA6Gb7EuIx6r48aIXsRTQizRz9H2BWba5OnNAjasJSSFbWr/HbAbhdmn18B6xPsXJvNn2r colin@anatinus'
+    ]
+  }
+]
