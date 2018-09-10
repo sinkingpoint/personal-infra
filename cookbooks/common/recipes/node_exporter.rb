12b43a6ddad6ea7ab4255222d043184e87d3621b
sinkingpoint <colin@quirl.co.nz>
2018-09-10 19:44:17 +0100

    Added textfile directory to node exporter

--- cookbooks/common/recipes/node_exporter.rb
+++ cookbooks/common/recipes/node_exporter.rb
@@ -1,4 +1,5 @@
 node_exporter 'main' do
   web_listen_address "#{node['exporters']['listen_ip']}:9100"
+  collector_textfile_directory node['prometheus']['node_exporter']['dir']
   action [:enable, :start]
 end
