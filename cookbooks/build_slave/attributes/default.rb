0cb8a2e00df4699cdfb7ff5d942c344382d3715b
sinkingpoint <colin@quirl.co.nz>
2018-09-10 22:46:01 +0100

    Change shell to bash for jenkins

--- cookbooks/build_slave/attributes/default.rb
+++ cookbooks/build_slave/attributes/default.rb
@@ -1,7 +1,7 @@
 default['jenkins']['users'] = [
   {
     name: 'jenkins',
-    shell: '/usr/bin/bash',
+    shell: '/bin/bash',
     ssh_keys: [
       'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDXiZ+mJvyM1a5w6budjFObnPS7FD1fkw5ejWiAPU/CbmOhwsBh/KFhVlwJX6MIA8N21pQN4vS4XKxZMan4fhrLhOkayNhy5FYOcBzq7ChcdgqdKaQXjEkorQlzWFFWyMP0hRiFXaDmsAxPrF8WGZPbJ+1JsxAm1w03C1m5YcdKtS6NowEzueAFUzp1jix5iCscmHm6NN+gpiM4+kMMtzbUf5SNvCY0Hxvf4C9jWq7XBX+P9/o2Jc00HJczfq1DG3wFpSo50mRNMEbMz5TA6Gb7EuIx6r48aIXsRTQizRz9H2BWba5OnNAjasJSSFbWr/HbAbhdmn18B6xPsXJvNn2r colin@anatinus'
     ]
