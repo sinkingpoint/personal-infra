6b5e727b99d7c1146c9897a97c0a2af426e005de
sinkingpoint <colin@quirl.co.nz>
2018-10-02 15:49:47 +0100

    Added attribute for prometheus internal IP

--- cookbooks/prometheus/attributes/default.rb
+++ cookbooks/prometheus/attributes/default.rb
@@ -1,5 +1,5 @@
 interface = node['network']['interfaces']['eth0'] || node['network']['interfaces']['eno1']
-default['exporters']['listen_ip'] = (interface['addresses'].select { |a, details| details[:family] == 'inet'}).keys[0]
+default['prometheus']['exporters']['listen_ip'] = (interface['addresses'].select { |a, details| details[:family] == 'inet'}).keys[0]
 override['nginx']['servers'] = [
   {
     name: 'prometheus.sinkingpoint.com',
