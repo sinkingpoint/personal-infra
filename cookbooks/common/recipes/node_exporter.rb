4571d7d4776ff6e605063682c0b32a1568c352c6
sinkingpoint <colin@quirl.co.nz>
2019-01-19 20:38:44 +0000

    Change node exporter directory

--- cookbooks/common/recipes/node_exporter.rb
+++ cookbooks/common/recipes/node_exporter.rb
@@ -1,5 +1,5 @@
 node_exporter 'main' do
   web_listen_address "#{node['exporters']['listen_ip']}:9100"
-  collector_textfile_directory node['prometheus']['node_exporter']['dir']
+  collector_textfile_directory ::File.join(node['prometheus']['node_exporter']['dir'], 'metrics')
   action [:enable, :start]
 end
