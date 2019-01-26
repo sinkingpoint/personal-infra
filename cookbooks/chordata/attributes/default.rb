01b9ed15c048e0dad13a87c339ff1f19f7dd31f6
sinkingpoint <colin@quirl.co.nz>
2019-01-26 16:33:38 +0000

    Mount gitlab onto the local filesystem

--- cookbooks/chordata/attributes/default.rb
+++ cookbooks/chordata/attributes/default.rb
@@ -2,7 +2,7 @@ override['plex']['plex_mount_point'] = '/l1-za/plex'
 override['plex']['apps_mount_point'] = '/l1-za/storage/apps'
 override['plex']['media_mount_point'] = '/l1-za/storage/media'
 override['grafana']['mount_point'] = '/l1-za/storage/apps'
-override['gitlab']['mount_point'] = '/l1-za/storage/apps'
+override['gitlab']['mount_point'] = '/opt/gitlab'
 
 override['nginx']['servers'] = [
   {
